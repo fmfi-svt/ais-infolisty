94c9318a6c6ff0b8fee0e96aef500190a5d0c139
Kristián Valentín <valentin.kristian@gmail.com>
2013-10-26 15:58:46 +0200

    oprava skriptu na generovanie infolistov ku statnym skuskam

--- vygeneruj_statne-skusky.sh
+++ vygeneruj_statne-skusky.sh
@@ -2,5 +2,6 @@
 
 DIR="/var/www/sluzby/infolist"
 
-python "$DIR/scripts/AIS_XML2HTML_statne-skusky.py" "$DIR/xml_files_statne-skusky_sk/mgr/" "$DIR/public/SK";
-python "$DIR/scripts/AIS_XML2HTML_statne-skusky.py" "$DIR/xml_files_statne-skusky_sk/bc/" "$DIR/public/SK";
+# sk
+python "$DIR/scripts/AIS_XML2HTML_statne-skusky.py" "$DIR/scripts/xml_files_statne-skusky_sk/mgr/" "$DIR/public/SK";
+python "$DIR/scripts/AIS_XML2HTML_statne-skusky.py" "$DIR/scripts/xml_files_statne-skusky_sk/bc/" "$DIR/public/SK";
