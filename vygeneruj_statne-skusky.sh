8b39c41ca63776ca93c9cdeadd7c750827d8864c
Kristián Valentín <valentin.kristian@gmail.com>
2013-11-06 10:59:46 +0100

    pridane generovanie infolistov k statnym skuskam v anglickom jazyku

--- vygeneruj_statne-skusky.sh
+++ vygeneruj_statne-skusky.sh
@@ -5,3 +5,7 @@ DIR="/var/www/sluzby/infolist"
 # sk
 python "$DIR/scripts/AIS_XML2HTML_statne-skusky.py" "$DIR/scripts/xml_files_statne-skusky_sk/mgr/" "$DIR/public/SK";
 python "$DIR/scripts/AIS_XML2HTML_statne-skusky.py" "$DIR/scripts/xml_files_statne-skusky_sk/bc/" "$DIR/public/SK";
+
+# en
+python "$DIR/scripts/AIS_XML2HTML_statne-skusky.py" "$DIR/scripts/xml_files_statne-skusky_en/mgr/" --lang en "$DIR/public/EN";
+python "$DIR/scripts/AIS_XML2HTML_statne-skusky.py" "$DIR/scripts/xml_files_statne-skusky_en/bc/" --lang en "$DIR/public/EN";
