f4f477ec66212b42954cea92c5fc230e91daa06a
Tomas Vinar <tvinar@gmail.com>
2026-04-02 09:29:15 +0200

    register zamestnancov update

--- portal-vs/portal-vs.pl
+++ portal-vs/portal-vs.pl
@@ -4,20 +4,20 @@ use utf8;
 
 my $url = "https://www.portalvs.sk/regzam/?do=filterForm-submit&university=701000000&sort=surname&employment_state=yes";
 
-`wget -O temp.txt --keep-session-cookies --save-cookies cookies.txt`;
+`wget -O temp.txt --keep-session-cookies --save-cookies cookies.txt $url`;
 
 my $meno = "";
 my $linka = "";
 my $fakulta = "";
 binmode STDOUT,":utf8";
 
-for (my $i=1; $i<63; $i++) {
+for (my $i=1; $i<72; $i++) {
     unlink("temp.txt");
     `wget -O temp.txt --load-cookies cookies.txt '$url&vp-page=$i'`;
     open IN,"<:encoding(UTF-8)","temp.txt";
     
     while (my $line=<IN>) {
-	if ($line =~ /\<a href=\"(\/regzam\/detail\/.*)\?do=.*>(.*)<\/a>/) {
+	if ($line =~ /\<a title=\"Aktuálny výpis\" href=\"(\/regzam\/detail\/.*)\?do=.*>(.*)<\/a>/) {
 	    if ($meno) {
 		print join("\t",$meno,$linka,$fakulta),"\n";
 	    }
